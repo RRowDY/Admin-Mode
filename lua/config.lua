80f58e45ed4d3ccc4b5169c8f3f96d76d1f71792
RRowDY <durellwalzer@gmail.com>
2020-12-03 16:00:43 -0700

    Added a commented out template for the groupsAllowed table

--- lua/config.lua
+++ lua/config.lua
@@ -2,10 +2,9 @@ adminSys = adminSys or {}
 
 adminSys.consoleCommand = "adminmode" -- The console command for the admin system. Requires server restart or map reload.
 
-adminSys.groupsAllowed = { -- Usergroups allowed to use the console command.
-  ["admin"] = true,
-  ["mod"] = true,
-  ["smod"] = true,
-  ["tmod"] = true,
-  ["gamemaster"] = true,
+adminSys.groupsAllowed = { -- Usergroups allowed to use the console command. Follow the format below.
+  ["superadmin"] = true,
+  -- ["admin"] = true,
+  -- ["mod"] = true,
+  -- ["tmod"] = true,
 }
