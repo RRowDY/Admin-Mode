0990b95c3c678e69fc874d341a861edac27cbef9
RRowDY <durellwalzer@gmail.com>
2020-12-03 15:58:56 -0700

    Added extra comment info

--- lua/config.lua
+++ lua/config.lua
@@ -1,6 +1,6 @@
 adminSys = adminSys or {}
 
-adminSys.consoleCommand = "adminmode" -- The console command for the admin system.
+adminSys.consoleCommand = "adminmode" -- The console command for the admin system. Requires server restart or map reload.
 
 adminSys.groupsAllowed = { -- Usergroups allowed to use the console command.
   ["admin"] = true,
