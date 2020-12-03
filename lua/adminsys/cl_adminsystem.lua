79db50d6dfd033e22031777d9a8a03d4b1236628
RRowDY <durellwalzer@gmail.com>
2020-12-03 16:39:50 -0700

    Add a check to not send the message to non-usergroup players.

--- lua/adminsys/cl_adminsystem.lua
+++ lua/adminsys/cl_adminsystem.lua
@@ -1,4 +1,6 @@
 net.Receive("adminSystem_chatNotify", function(len)
+  local ply = LocalPlayer()
+  if not adminSys.groupsAllowed[ply:GetUserGroup()] then return end
   local txt = net.ReadString()
   chat.AddText(Color(231, 76, 60), "| ",  Color(46, 204, 113), "You ", Color(255,255,255), txt)
 end)
