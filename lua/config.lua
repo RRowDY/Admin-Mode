2054274407bcfcc92d2218adce9eca5158b9c48f
RRowDY <durellwalzer@gmail.com>
2020-12-06 02:38:24 -0700

    Added a prefix and chatCommand config setting

--- lua/config.lua
+++ lua/config.lua
@@ -2,6 +2,10 @@ adminSys = adminSys or {}
 
 adminSys.consoleCommand = "adminmode" -- The console command for the admin system. Requires server restart or map reload.
 
+adminSys.prefix = "~" -- The prefix for the chat command. Requires server restart or map reload.
+
+adminSys.chatCommand = "adminmode" -- The chat command for the admin system. Requires server restart or map reload.
+
 adminSys.groupsAllowed = { -- Usergroups allowed to use the console command. Follow the format below.
   ["superadmin"] = true,
   -- ["admin"] = true,
